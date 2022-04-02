a889855c6b3333f7a76340c01ef072fc17ec0bf9
tripledawg <iamlesliecrouch>
2022-04-02 15:08:22 -0400

    moved songs into server folder and added connection code to connection.js and server.js

--- songs-server/server.js
+++ songs-server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const db = require('./config/connection');
+const db = require('./config/connection');
 // const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(routes);
 
-// db.once('open', () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for social network api running on port ${PORT}!`);
   });
-// });
+});
 
