9a6851e5d43337362fd1a3ee3d4926702ca26055
tripledawg <iamlesliecrouch>
2022-04-02 12:25:20 -0400

    restructured file organization

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Game from './components/Game';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+    <Nav />
+    <Game />
+  </div>
+  );
+}
+
+export default App;
