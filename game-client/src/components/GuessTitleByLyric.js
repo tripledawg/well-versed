f125f50727227fe837d9c8d1ba9549770ba378a1
tripledawg <iamlesliecrouch>
2022-04-05 18:16:33 -0400

    added state and change handlers for radio buttons

--- game-client/src/components/GuessTitleByLyric.js
+++ game-client/src/components/GuessTitleByLyric.js
@@ -18,6 +18,25 @@ function shuffleArray(array) {
 function GuessTitleByLyric({ lyric }) {
   const [data, setData] = React.useState(null);
   const [randomizedData, setRandomizedData] = React.useState(null);
+  const [value, setValue] = React.useState(null);
+  const handleChange = () => {
+    setValue(false);
+  };
+  const handleAnswer1Change = () => {
+    setValue("answer1");
+  };
+  const handleAnswer2Change = () => {
+    setValue("answer2");
+  }; 
+  const handleAnswer3Change = () => {
+    setValue("answer3");
+  }; 
+  const handleAnswer4Change = () => {
+    setValue("answer4");
+  }; 
+  const handleAnswer5Change = () => {
+    setValue("answer5");
+  };
   React.useEffect(() => {
     fetch("/api/songLyrics")
       .then((res) => res.json())
@@ -35,11 +54,11 @@ function GuessTitleByLyric({ lyric }) {
     <div id="background">
       <h3>What song is this lyric from?</h3>
       <p>{!data ? "loading..." : data.aggregation[0].lyric}</p>
-      <label><input type="radio" /> {!randomizedData ? "loading..." : randomizedData[0].title} by {!randomizedData ? "loading..." : randomizedData[0].artist}</label><br></br>
-      <label><input type="radio" /> {!randomizedData ? "loading..." : randomizedData[1].title} by {!randomizedData ? "loading..." : randomizedData[1].artist}</label><br></br>
-      <label><input type="radio" /> {!randomizedData ? "loading..." : randomizedData[2].title} by {!randomizedData ? "loading..." : randomizedData[2].artist}</label><br></br>
-      <label><input type="radio" /> {!randomizedData ? "loading..." : randomizedData[3].title} by {!randomizedData ? "loading..." : randomizedData[3].artist}</label><br></br>
-      <label><input type="radio" /> {!randomizedData ? "loading..." : randomizedData[4].title} by {!randomizedData ? "loading..." : randomizedData[4].artist}</label><br></br>
+      <label><input type="radio" checked={value === "answer1"} onChange={handleAnswer1Change} /> {!randomizedData ? "loading..." : randomizedData[0].title} by {!randomizedData ? "loading..." : randomizedData[0].artist}</label><br></br>
+      <label><input type="radio" checked={value === "answer2"} onChange={handleAnswer2Change} /> {!randomizedData ? "loading..." : randomizedData[1].title} by {!randomizedData ? "loading..." : randomizedData[1].artist}</label><br></br>
+      <label><input type="radio" checked={value === "answer3"} onChange={handleAnswer3Change} /> {!randomizedData ? "loading..." : randomizedData[2].title} by {!randomizedData ? "loading..." : randomizedData[2].artist}</label><br></br>
+      <label><input type="radio" checked={value === "answer4"} onChange={handleAnswer4Change} /> {!randomizedData ? "loading..." : randomizedData[3].title} by {!randomizedData ? "loading..." : randomizedData[3].artist}</label><br></br>
+      <label><input type="radio" checked={value === "answer5"} onChange={handleAnswer5Change} /> {!randomizedData ? "loading..." : randomizedData[4].title} by {!randomizedData ? "loading..." : randomizedData[4].artist}</label><br></br>
     </div>
   );
 }
