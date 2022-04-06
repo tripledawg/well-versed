6c92a2e9f938d95d30321f37ca52b8f85d036b01
tripledawg <iamlesliecrouch>
2022-04-05 21:19:51 -0400

    added Submit button and started onSubmit

--- game-client/src/components/GuessTitleByLyric.js
+++ game-client/src/components/GuessTitleByLyric.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './GuessTitleByLyric.css';
 
-/* Randomize array in-place using Durstenfeld shuffle algorithm */
+/* Randomize nultiple choice answer array in-place using Durstenfeld shuffle algorithm */
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -18,22 +18,22 @@ function shuffleArray(array) {
 function GuessTitleByLyric({ lyric }) {
   const [data, setData] = React.useState(null);
   const [randomizedData, setRandomizedData] = React.useState(null);
-  const [value, setValue] = React.useState(null);
-  const handleChange = () => {
-    setValue(false);
-  };
+  const [value, setValue] = React.useState(null);  //prevents a radio button from being selected at outset
+  // const handleChange = () => {
+  //   setValue(false);
+  // };
   const handleAnswer1Change = () => {
     setValue("answer1");
   };
   const handleAnswer2Change = () => {
     setValue("answer2");
-  }; 
+  };
   const handleAnswer3Change = () => {
     setValue("answer3");
-  }; 
+  };
   const handleAnswer4Change = () => {
     setValue("answer4");
-  }; 
+  };
   const handleAnswer5Change = () => {
     setValue("answer5");
   };
@@ -41,26 +41,43 @@ function GuessTitleByLyric({ lyric }) {
     fetch("/api/songLyrics")
       .then((res) => res.json())
       .then((data) => {
-        setData(data);
+        setData(data);  //all raw data 
         var randomArray = [];
-        for (let i = 0; i < data.aggregation.length; i++) {
+        for (let i = 0; i < data.aggregation.length; i++) {   //array is called "aggregation"
           randomArray[i] = data.aggregation[i];
         }
         shuffleArray(randomArray);
         setRandomizedData(randomArray);
       });
   }, []);
+
+  const onSubmit = (e) => {
+    console.log("reached on submit")
+    e.preventDefault();
+    if (data.aggregation[0].title + ' by ' + data.aggregation[0].artist === e) {
+      // display feedback
+      // assign and store points
+      // draw a new component with a new question
+      // check and increment the question counter or end game and post final score and user signup prompt
+    }
+  };
+
+
   return (
     <div id="background">
-      <h3>What song is this lyric from?</h3>
-      <p>{!data ? "loading..." : data.aggregation[0].lyric}</p>
-      <label><input type="radio" checked={value === "answer1"} onChange={handleAnswer1Change} /> {!randomizedData ? "loading..." : randomizedData[0].title} by {!randomizedData ? "loading..." : randomizedData[0].artist}</label><br></br>
-      <label><input type="radio" checked={value === "answer2"} onChange={handleAnswer2Change} /> {!randomizedData ? "loading..." : randomizedData[1].title} by {!randomizedData ? "loading..." : randomizedData[1].artist}</label><br></br>
-      <label><input type="radio" checked={value === "answer3"} onChange={handleAnswer3Change} /> {!randomizedData ? "loading..." : randomizedData[2].title} by {!randomizedData ? "loading..." : randomizedData[2].artist}</label><br></br>
-      <label><input type="radio" checked={value === "answer4"} onChange={handleAnswer4Change} /> {!randomizedData ? "loading..." : randomizedData[3].title} by {!randomizedData ? "loading..." : randomizedData[3].artist}</label><br></br>
-      <label><input type="radio" checked={value === "answer5"} onChange={handleAnswer5Change} /> {!randomizedData ? "loading..." : randomizedData[4].title} by {!randomizedData ? "loading..." : randomizedData[4].artist}</label><br></br>
+      <form onSubmit={onSubmit}>
+        <h3>What song is this lyric from?</h3>
+        <p>{!data ? "loading..." : data.aggregation[0].lyric}</p>
+        <label><input type="radio" checked={value === "answer1"} onChange={handleAnswer1Change} /> {!randomizedData ? "loading..." : randomizedData[0].title} by {!randomizedData ? "loading..." : randomizedData[0].artist}</label><br></br>
+        <label><input type="radio" checked={value === "answer2"} onChange={handleAnswer2Change} /> {!randomizedData ? "loading..." : randomizedData[1].title} by {!randomizedData ? "loading..." : randomizedData[1].artist}</label><br></br>
+        <label><input type="radio" checked={value === "answer3"} onChange={handleAnswer3Change} /> {!randomizedData ? "loading..." : randomizedData[2].title} by {!randomizedData ? "loading..." : randomizedData[2].artist}</label><br></br>
+        <label><input type="radio" checked={value === "answer4"} onChange={handleAnswer4Change} /> {!randomizedData ? "loading..." : randomizedData[3].title} by {!randomizedData ? "loading..." : randomizedData[3].artist}</label><br></br>
+        <label><input type="radio" checked={value === "answer5"} onChange={handleAnswer5Change} /> {!randomizedData ? "loading..." : randomizedData[4].title} by {!randomizedData ? "loading..." : randomizedData[4].artist}</label><br></br>
+
+        <button type="submit">Submit</button> </form>  {/*added */}
     </div>
   );
 }
+
 
 export default GuessTitleByLyric;
