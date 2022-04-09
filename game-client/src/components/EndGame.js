4ca9b89f451e3ed4764001b73f9b1807041b1fe4
tripledawg <iamlesliecrouch>
2022-04-09 11:38:37 -0400

    play again logic resetting score and question set

--- game-client/src/components/EndGame.js
+++ game-client/src/components/EndGame.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './EndGame.css';
 
 export default function EndGame(props) {
+  const resetGame = () => {
+    props.setQuestionCount(0);
+    props.setScore(0);
+  }
   return (
   <div><p>Game Over! Final Score: {props.score}</p>
-  {/* <a>Sign up for custom games?</a>
-  <a>Play Again</a> */}
+  {/* <button>Sign up for custom games?</button> */}
+  <button onClick={resetGame}>Play Again</button>
   </div>    
   );
 }
